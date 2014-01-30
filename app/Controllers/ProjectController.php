0bcf3f672081b82dc0bc4e750bf95d1e9fbd1999
hack1337 <contact@web-infocom.fr>
2014-01-30 22:02:04 +0100

    [ADD] create/list project

--- app/Controllers/ProjectController.php
+++ app/Controllers/ProjectController.php
@@ -13,15 +13,30 @@ class ProjectController extends Controller {
 	}
 
 	public function create ($f3) {
-		foreach ($_FILES as $file) {
-			echo $file['name'];
-		}
-		// if ((isset($_FILES['photo1']['tmp_name']))) {   
-		// 	move_uploaded_file($_FILES['photo1']['tmp_name'], $f3->get('UPLOADS').$_FILES['photo1']['name']);     
-		// } 
-		// $this->model->create();
-		// echo 'Projet sended';
+		$f3->set('photos', array());
+		if (Web::instance()->receive(
+			function($file) use($f3) {
+				$photos = $f3->get('photos');
+				array_push($photos, $file['name']);
+				$f3->set('photos', $photos);
+				$allowed=$f3->get('allowed');
+				return is_array($allowed) && in_array($file['type'],$allowed);
+			}
+		))
+
+		$begin = mktime (0,0,0, date("n"), date("j")+1, date("Y") );
+		$last = mktime (0,0,0, date("n"), date("j")+14, date("Y") );
+
+		$this->model->create(array('photos' => serialize($f3->get('photos')), 'begin' => $begin, 'last' => $last, 'id' => $f3->get('SESSION.id') ));
+		$date = new DateTime();
+		echo 'Projet Ajouté';
 		$this->content = 'newProject';
+	}
+
+	public function liste ($f3) {
+		$res = $this->model->liste(array('id' => $f3->get('SESSION.id')));
+		$f3->set('liste', $res);
+		$this->content = 'listProject';
 	}
 
 }
--- app/Models/ProjectModel.php
+++ app/Models/ProjectModel.php
@@ -9,10 +9,17 @@ private $mapper;
 		$this->mapper=$this->getMapper('projects');
 	}
 
-	public function create () {
+	public function create ($params) {
   		$this->mapper->copyFrom('POST');
-  		$this->mapper->photos = $_FILES['photo1']['name'];
+  		$this->mapper->photos = $params['photos'];
+  		$this->mapper->beginDay = $params['begin'];
+  		$this->mapper->lastDay = $params['last'];
+  		$this->mapper->id_user = $params['id'];
 		$this->mapper->save();
+	}
+
+	public function liste ($params) {
+		return $this->mapper->find('id_user = "'.$params['id'].'"');
 	}
 }
 
