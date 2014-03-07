47e01b52b8ee188ae37ae071210851e64b8d3411
Aurelsicoko <aurelsicoko@gmail.com>
2014-03-07 15:36:25 +0100

    Finition link/route

--- index.php
+++ index.php
@@ -6,6 +6,10 @@ $f3->config('config/config.ini');
 
 $f3->config('config/routes.ini');
 
+$f3->set('ONERROR',function($f3){
+    echo \View::instance()->render('404.html');
+});
+
 $f3->run();
 
 ?>
